55d4a0abfe6fa8ddac81aa3bc92a0945c3ffa129
Tran Vu <vu.tranhoang@elca.vn>
2024-09-17 19:02:10 +0700

    handling navigation when refreshToken is null

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,11 +1,13 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import { authService } from './../service/AuthService';
+import { useNavigate } from 'react-router-dom';
 
 const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const initAuth = async () => {
@@ -37,6 +39,7 @@ export const AuthProvider = ({ children }) => {
   const logout = async () => {
     await authService.signout();
     setUser(null);
+    navigate('/signin')
   };
 
   const value = {
