90c10357d3273ede2c400d09bf781e2d31b70b58
Tran Vu <vu.tranhoang@elca.vn>
2024-09-18 22:21:13 +0700

    allow toggles for receptionist only

--- src/fragment/ordermanagement/OrderDetails.js
+++ src/fragment/ordermanagement/OrderDetails.js
@@ -700,10 +700,12 @@ const OrderDetails = ({ orderData, onUpdate }) => {
         <div className="mt-6">
           <div className="flex items-center justify-between wrap">
             <h3 className="text-lg font-semibold">Sprayer Management</h3>
-            <label className="inline-flex items-center cursor-pointer">
+            {user.roles.includes('ROLE_RECEPTIONIST') && (
+              <label className="inline-flex items-center cursor-pointer">
               <input type="checkbox" checked={order.autoAssign} className="sr-only peer" disabled={!isEditing} onChange={(e) => handleAutoAssignChange(e.target.checked)} />
               <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             </label>
+            )}
           </div>
           <div className="flex mb-4 border-b">
             <button
